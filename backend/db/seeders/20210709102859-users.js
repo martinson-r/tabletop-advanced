6691dff9943bdbb03bebde739dbf8265ba7a4b3c
Rihana Chang <martinson.r@gmail.com>
2021-08-06 11:00:55 -0700

    Add seed data

--- backend/db/seeders/20210709102859-users.js
+++ backend/db/seeders/20210709102859-users.js
@@ -28,7 +28,55 @@ module.exports = {
         hashedPassword: bcrypt.hashSync('password123', 10),
         isAdmin: false,
         hideStrangers: false
-        }
+        },
+        {
+          email: 'thegrumblinggrognard@example.com',
+          userName: 'GrumblingGrognard',
+          emailVerified: true,
+          hashedPassword: bcrypt.hashSync('password123', 10),
+          isAdmin: false,
+          hideStrangers: false
+          },
+          {
+            email: 'coffeelikemud@example.com',
+            userName: 'Coffee_Like_Mud',
+            emailVerified: true,
+            hashedPassword: bcrypt.hashSync('password123', 10),
+            isAdmin: false,
+            hideStrangers: false
+            },
+            {
+              email: 'billyboy@example.com',
+              userName: 'CyborgGoat',
+              emailVerified: true,
+              hashedPassword: bcrypt.hashSync('password123', 10),
+              isAdmin: false,
+              hideStrangers: false
+              },
+              {
+                email: 'damien@example.com',
+                userName: 'xxDarkOverLordxx',
+                emailVerified: true,
+                hashedPassword: bcrypt.hashSync('password123', 10),
+                isAdmin: false,
+                hideStrangers: false
+                },
+                {
+                  email: 'mercedes@example.com',
+                  userName: 'notmercedeslackey',
+                  emailVerified: true,
+                  hashedPassword: bcrypt.hashSync('password123', 10),
+                  isAdmin: false,
+                  hideStrangers: false
+                  },
+                  {
+                    email: 'hardcoremath@example.com',
+                    userName: 'HardcoreMath',
+                    emailVerified: true,
+                    hashedPassword: bcrypt.hashSync('password123', 10),
+                    isAdmin: false,
+                    hideStrangers: false
+                    }
     ], {});
   },
 
